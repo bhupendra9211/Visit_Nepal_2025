139c3647ce4c97f239be6ba6ec2c1797a421a4bb
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-08-26 23:14:27 +0545

    added show and update action

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,32 @@
 class Api::V1::PlacesController < ApplicationController
+  before_action :set_place, only: %i[show update]
   def index
     @places = Place.all
     render json: @places
   end
+  def show
+    render json: @place
+  end
+
+  def update
+    if @place.update(place_params)
+      render json: @place
+    else
+      render json: @place.errors, status: :unprocessable_entity
+      # render json: @place.errors.messages, status: :unprocessable_entity
+      # render json: @place.errors.full_messages, status: :unprocessable_entity
+      # render json: {errors: @place.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :latitude, :longitude, :city, :state, :country, :image_url)
+  end
+
 end
